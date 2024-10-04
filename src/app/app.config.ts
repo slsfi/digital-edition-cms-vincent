ced0b506a69d14a492e32ea2a390fa76fb6ed94e
Tomi Kiiski <tokiiski@gmail.com>
2024-10-04 14:29:45 +0300

    Enable material animations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimationsAsync('noop'),
+    provideAnimationsAsync('animations'),
     provideHttpClient(
       withInterceptors([authInterceptor]),
     ),
