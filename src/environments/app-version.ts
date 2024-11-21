e1aebac15432decc5cf112062cfe33ba265c8fe8
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2024-11-21 15:41:27 +0200

    feature: display app version dynamically on home page
    In the production build, the app version is read from `package.json` and stored in a ts-file, which the app imports.

---
+++ src/environments/app-version.ts
@@ -0,0 +1,6 @@
+// In the production build, this file will be replaced
+// by the generated `app-version.generated.ts`, where
+// APP_VERSION is read from `package.json`.
+// See the prebuild script `prebuild-generate-app-version.ts`
+// for further details.
+export const APP_VERSION = '0.0.0';
