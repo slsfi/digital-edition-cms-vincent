slsfi/digital-edition-cms-vincent

commit bd0e088553afa67faf939d367715a2a8db6a4672

merge commit for the refactoring selectedProject-reactive dependencies

src/app/components/publications/publications.component.ts
@@ -27,6 +27,7 @@ import { LoadingSpinnerComponent } from '../loading-spinner/loading-spinner.comp
 import { TableFiltersComponent } from '../table-filters/table-filters.component';
 import { TableSortingComponent } from '../table-sorting/table-sorting.component';
 import { LoadingService } from '../../services/loading.service';
+import { ProjectService } from '../../services/project.service';
 import { PublicationService } from '../../services/publication.service';
 import { QueryParamsService } from '../../services/query-params.service';
 import { Column, Deleted } from '../../models/common';
@@ -83,6 +84,7 @@ export class PublicationsComponent implements OnInit {
 
   constructor(
     private publicationService: PublicationService,
+    private projectService: ProjectService,
     private route: ActivatedRoute,
     private dialog: MatDialog,
     private queryParamsService: QueryParamsService,
@@ -106,7 +108,10 @@ export class PublicationsComponent implements OnInit {
     this.publications$ = this.publicationsLoader$.asObservable().pipe(
       switchMap(() => combineLatest([this.selectedProject$, this.publicationCollectionId$]).pipe(
         distinctUntilChanged(([, prevCollectionId], [, nextCollectionId]) => prevCollectionId === nextCollectionId),
-        switchMap(([, collectionId]) => this.publicationService.getPublications(collectionId as string))
+        switchMap(([project, collectionId]) => {
+          if (!project) { return of([]); }
+          return this.publicationService.getPublications(collectionId as string, project);
+        })
       ))
     );
 
@@ -115,30 +120,42 @@ export class PublicationsComponent implements OnInit {
     );
 
     this.comments$ = this.commentLoader$.asObservable().pipe(
-      switchMap(() => combineLatest([this.publicationCollectionId$, this.publicationId$]).pipe(
+      switchMap(() => combineLatest([this.selectedProject$, this.publicationCollectionId$, this.publicationId$]).pipe(
         distinctUntilChanged(([, prevCollectionId], [, nextCollectionId]) => prevCollectionId === nextCollectionId),
-        switchMap(([collectionId, publicationId]) => this.publicationService.getCommentsForPublication(collectionId as string, publicationId as string))
+        switchMap(([project, collectionId, publicationId]) => {
+          if (!project) { return of([]); }
+          return this.publicationService.getCommentsForPublication(collectionId as string, publicationId as string, project);
+        })
       ))
     );
 
     this.versions$ = this.versionsLoader$.asObservable().pipe(
-      switchMap(() => combineLatest([this.publicationCollectionId$, this.publicationId$]).pipe(
+      switchMap(() => combineLatest([this.selectedProject$, this.publicationCollectionId$, this.publicationId$]).pipe(
         distinctUntilChanged(([, prevCollectionId], [, nextCollectionId]) => prevCollectionId === nextCollectionId),
-        switchMap(([, publicationId]) => this.publicationService.getVersionsForPublication(publicationId as string))
+        switchMap(([project, , publicationId]) => {
+          if (!project) { return of([]); }
+          return this.publicationService.getVersionsForPublication(publicationId as string, project);
+        })
       ))
     );
 
     this.manuscripts$ = this.manuscriptsLoader$.asObservable().pipe(
-      switchMap(() => combineLatest([this.publicationCollectionId$, this.publicationId$]).pipe(
+      switchMap(() => combineLatest([this.selectedProject$, this.publicationCollectionId$, this.publicationId$]).pipe(
         distinctUntilChanged(([, prevCollectionId], [, nextCollectionId]) => prevCollectionId === nextCollectionId),
-        switchMap(([, publicationId]) => this.publicationService.getManuscriptsForPublication(publicationId as string))
+        switchMap(([project, , publicationId]) => {
+          if (!project) { return of([]); }
+          return this.publicationService.getManuscriptsForPublication(publicationId as string, project);
+        })
       ))
     );
 
     this.facsimiles$ = this.facsimilesLoader$.asObservable().pipe(
-      switchMap(() => combineLatest([this.publicationCollectionId$, this.publicationId$]).pipe(
+      switchMap(() => combineLatest([this.selectedProject$, this.publicationCollectionId$, this.publicationId$]).pipe(
         distinctUntilChanged(([, prevCollectionId], [, nextCollectionId]) => prevCollectionId === nextCollectionId),
-        switchMap(([, publicationId]) => this.publicationService.getFacsimilesForPublication(publicationId as string))
+        switchMap(([project, , publicationId]) => {
+          if (!project) { return of([]); }
+          return this.publicationService.getFacsimilesForPublication(publicationId as string, project);
+        })
       ))
     );
   }
@@ -156,10 +173,11 @@ export class PublicationsComponent implements OnInit {
         let request$: Observable<PublicationResponse>;
         const formValue = result.form.value;
 
+        const currentProject = this.projectService.getCurrentProject();
         if (publication?.id) {
-          request$ = this.publicationService.editPublication(publication.id, formValue);
+          request$ = this.publicationService.editPublication(publication.id, formValue, currentProject);
         } else {
-          request$ = this.publicationService.addPublication(parseInt(collectionId), formValue).pipe(
+          request$ = this.publicationService.addPublication(parseInt(collectionId), formValue, currentProject).pipe(
             switchMap((response: PublicationResponse) => {
               const pub = response.data;
 
@@ -176,7 +194,7 @@ export class PublicationsComponent implements OnInit {
                 sort_order: 1
               };
 
-              return this.publicationService.linkTextToPublication(pub.id, manuscriptPayload).pipe(
+              return this.publicationService.linkTextToPublication(pub.id, manuscriptPayload, currentProject).pipe(
                 take(1),
                 map(() => response)  // preserve the original response
               );
@@ -219,7 +237,8 @@ export class PublicationsComponent implements OnInit {
       const filePath = result.join('/');
       const data = { original_filename: filePath };
 
-      this.publicationService.editPublication(publication.id, data).pipe(take(1)).subscribe({
+      const currentProject = this.projectService.getCurrentProject();
+      this.publicationService.editPublication(publication.id, data, currentProject).pipe(take(1)).subscribe({
         next: () => {
           this.publicationsLoader$.next(0);
           this.snackbar.open('File path saved', 'Close', { panelClass: ['snackbar-success'] });
@@ -242,16 +261,17 @@ export class PublicationsComponent implements OnInit {
         payload['text_type'] = 'version';
 
         let request$: Observable<VersionResponse | LinkTextToPublicationResponse>;
+        const currentProject = this.projectService.getCurrentProject();
         if (version?.id) {
           // Edit existing variant
           // Convert empty strings in field values to null in the payload object
           payload = cleanEmptyStrings(payload);
-          request$ = this.publicationService.editVersion(version.id, payload);
+          request$ = this.publicationService.editVersion(version.id, payload, currentProject);
         } else {
           // Add new variant
           // Remove empty fields from the payload object
           payload = cleanObject(payload);
-          request$ = this.publicationService.linkTextToPublication(publicationId, payload);
+          request$ = this.publicationService.linkTextToPublication(publicationId, payload, currentProject);
         }
         request$.pipe(take(1)).subscribe({
           next: () => {
@@ -277,16 +297,17 @@ export class PublicationsComponent implements OnInit {
         payload['text_type'] = 'manuscript';
 
         let request$: Observable<ManuscriptResponse | LinkTextToPublicationResponse>;
+        const currentProject = this.projectService.getCurrentProject();
         if (manuscript?.id) {
           // Edit existing manuscript
           // Convert empty strings in field values to null in the payload object
           payload = cleanEmptyStrings(payload);
-          request$ = this.publicationService.editManuscript(manuscript.id, payload);
+          request$ = this.publicationService.editManuscript(manuscript.id, payload, currentProject);
         } else {
           // Add new manuscript
           // Remove empty fields from the payload object
           payload = cleanObject(payload);
-          request$ = this.publicationService.linkTextToPublication(publicationId, payload);
+          request$ = this.publicationService.linkTextToPublication(publicationId, payload, currentProject);
         }
         request$.pipe(take(1)).subscribe({
           next: () => {
@@ -312,15 +333,16 @@ export class PublicationsComponent implements OnInit {
         payload['text_type'] = 'comment';
 
         let request$: Observable<PublicationCommentResponse | LinkTextToPublicationResponse>;
+        const currentProject = this.projectService.getCurrentProject();
         if (comment?.id) {
           // Edit existing comment
           // Convert empty strings in field values to null in the payload object
           payload = cleanEmptyStrings(payload);
-          request$ = this.publicationService.editComment(publicationId, payload);
+          request$ = this.publicationService.editComment(publicationId, payload, currentProject);
         } else {
           // Add new comment
           // Remove empty fields from the payload object
-          request$ = this.publicationService.linkTextToPublication(publicationId, payload);
+          request$ = this.publicationService.linkTextToPublication(publicationId, payload, currentProject);
         }
         request$.pipe(take(1)).subscribe({
           next: () => {
@@ -350,10 +372,11 @@ export class PublicationsComponent implements OnInit {
         const payload = result.form.getRawValue();
 
         let request$: Observable<LinkFacsimileToPublicationResponse>;
+        const currentProject = this.projectService.getCurrentProject();
         if (facsimile?.id) {
-          request$ = this.publicationService.editFacsimileForPublication(payload);
+          request$ = this.publicationService.editFacsimileForPublication(payload, currentProject);
         } else {
-          request$ = this.publicationService.linkFacsimileToPublication(publicationId, result.form);
+          request$ = this.publicationService.linkFacsimileToPublication(publicationId, result.form, currentProject);
         }
         request$.pipe(take(1)).subscribe({
           next: () => {
@@ -377,7 +400,8 @@ export class PublicationsComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       if (result?.value) {
         const payload = { id: facsimile.id, deleted: Deleted.Deleted };
-        this.publicationService.editFacsimileForPublication(payload).pipe(take(1)).subscribe({
+        const currentProject = this.projectService.getCurrentProject();
+        this.publicationService.editFacsimileForPublication(payload, currentProject).pipe(take(1)).subscribe({
           next: () => {
             this.facsimilesLoader$.next(0);
             this.snackbar.open('Facsimile deleted', 'Close', { panelClass: ['snackbar-success'] });
@@ -399,7 +423,8 @@ export class PublicationsComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       if (result?.value) {
         const payload = { deleted: Deleted.Deleted };
-        this.publicationService.editManuscript(manuscript.id, payload).pipe(take(1)).subscribe({
+        const currentProject = this.projectService.getCurrentProject();
+        this.publicationService.editManuscript(manuscript.id, payload, currentProject).pipe(take(1)).subscribe({
           next: () => {
             this.manuscriptsLoader$.next(0);
             this.snackbar.open('Manuscript deleted', 'Close', { panelClass: ['snackbar-success'] });
@@ -421,7 +446,8 @@ export class PublicationsComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       if (result?.value) {
         const payload = { deleted: Deleted.Deleted };
-        this.publicationService.editVersion(version.id, payload).pipe(take(1)).subscribe({
+        const currentProject = this.projectService.getCurrentProject();
+        this.publicationService.editVersion(version.id, payload, currentProject).pipe(take(1)).subscribe({
           next: () => {
             this.versionsLoader$.next(0);
             this.snackbar.open('Variant deleted', 'Close', { panelClass: ['snackbar-success'] });
@@ -443,14 +469,15 @@ export class PublicationsComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       if (result?.value) {
         const payload = { deleted: Deleted.Deleted };
-        this.publicationService.editComment(publicationId, payload).pipe(take(1)).subscribe({
+        const currentProject = this.projectService.getCurrentProject();
+        this.publicationService.editComment(publicationId, payload, currentProject).pipe(take(1)).subscribe({
           next: () => {
             this.commentLoader$.next(0);
             this.snackbar.open('Comment deleted', 'Close', { panelClass: ['snackbar-success'] });
           }
         });
 
-        this.publicationService.editPublication(publicationId, { publication_comment_id: null }).pipe(take(1)).subscribe({
+        this.publicationService.editPublication(publicationId, { publication_comment_id: null }, currentProject).pipe(take(1)).subscribe({
           next: () => {
             this.publicationsLoader$.next(0);
           }
@@ -473,7 +500,8 @@ export class PublicationsComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       if (result?.value) {
         const payload = { deleted: Deleted.Deleted, cascade_deleted: result.cascadeBoolean };
-        this.publicationService.editPublication(publication.id, payload).pipe(take(1)).subscribe({
+        const currentProject = this.projectService.getCurrentProject();
+        this.publicationService.editPublication(publication.id, payload, currentProject).pipe(take(1)).subscribe({
           next: () => {
             this.publicationsLoader$.next(0);
             this.snackbar.open('Publication deleted', 'Close', { panelClass: ['snackbar-success'] });
@@ -512,7 +540,8 @@ export class PublicationsComponent implements OnInit {
         this.metadataUpdating = true;
         this.metadataUpdateFailures = [];
 
-        this.publicationService.getPublications(collectionId, true).pipe(
+        const currentProject = this.projectService.getCurrentProject();
+        this.publicationService.getPublications(collectionId, currentProject).pipe(
           take(1),
           tap((publications: Publication[]) => {
             if (publications.length > 40) {
@@ -529,14 +558,14 @@ export class PublicationsComponent implements OnInit {
                   return of(null);
                 }
 
-                return this.publicationService.getMetadataFromXML(pub.original_filename, true).pipe(
+                return this.publicationService.getMetadataFromXML(pub.original_filename, currentProject).pipe(
                   take(1),
                   switchMap((metadata: XmlMetadata) => {
                     const updateRequest: PublicationEditRequest = {
                       ...metadata
                     };
 
-                    return this.publicationService.editPublication(pub.id, updateRequest, true).pipe(
+                    return this.publicationService.editPublication(pub.id, updateRequest, currentProject).pipe(
                       take(1)
                     );
                   }),

src/app/components/custom-table/custom-table.component.spec.ts
@@ -3,8 +3,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CustomTableComponent } from './custom-table.component';
 
 describe('CustomTableComponent', () => {
-  let component: CustomTableComponent;
-  let fixture: ComponentFixture<CustomTableComponent>;
+  let component: CustomTableComponent<any>;
+  let fixture: ComponentFixture<CustomTableComponent<any>>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -12,7 +12,7 @@ describe('CustomTableComponent', () => {
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(CustomTableComponent);
+    fixture = TestBed.createComponent(CustomTableComponent<any>);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });

src/app/components/edit-dialog/edit-dialog.component.spec.ts
@@ -2,9 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditDialogComponent } from './edit-dialog.component';
 
+// Simple interface for testing
+interface TestData {
+  id?: number;
+  name?: string;
+}
+
 describe('EditDialogComponent', () => {
-  let component: EditDialogComponent;
-  let fixture: ComponentFixture<EditDialogComponent>;
+  let component: EditDialogComponent<TestData>;
+  let fixture: ComponentFixture<EditDialogComponent<TestData>>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -12,7 +18,7 @@ describe('EditDialogComponent', () => {
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(EditDialogComponent);
+    fixture = TestBed.createComponent(EditDialogComponent<TestData>);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });

src/app/components/edit-dialog/edit-dialog.component.ts
@@ -18,6 +18,7 @@ import { TranslationsComponent } from '../translations/translations.component';
 import { Column, PublishedOptions } from '../../models/common';
 import { personTypeOptions } from '../../models/person';
 import { XmlMetadata } from './../../models/publication';
+import { ProjectService } from '../../services/project.service';
 import { PublicationService } from '../../services/publication.service';
 
 export interface EditDialogData<T> {
@@ -49,7 +50,10 @@ export interface EditDialogData<T> {
 })
 export class EditDialogComponent<T> implements OnInit {
 
-  constructor(private publicationService: PublicationService) { }
+  constructor(
+    private publicationService: PublicationService,
+    private projectService: ProjectService
+  ) { }
 
   readonly data = inject<EditDialogData<T>>(MAT_DIALOG_DATA);
 
@@ -202,7 +206,8 @@ export class EditDialogComponent<T> implements OnInit {
 
   getMetadata() {
     this.gettingMetadata = true;
-    this.publicationService.getMetadataFromXML(this.originalFilenameControl.value).pipe(
+    const currentProject = this.projectService.getCurrentProject();
+    this.publicationService.getMetadataFromXML(this.originalFilenameControl.value, currentProject).pipe(
       take(1),
       finalize(() => {
         this.gettingMetadata = false;

src/app/components/facsimile-file/facsimile-file.component.ts
@@ -6,6 +6,7 @@ import { BehaviorSubject, map } from 'rxjs';
 import { SkipLoading } from '../../interceptors/loading.interceptor';
 import { FacsimileService } from '../../services/facsimile.service';
 import { ApiService } from '../../services/api.service';
+import { ProjectService } from '../../services/project.service';
 
 @Component({
   selector: 'facsimile-file',
@@ -23,7 +24,11 @@ export class FacsimileFileComponent implements AfterViewInit {
   private imageUrlSubject = new BehaviorSubject<string>('');
   imageUrl$ = this.imageUrlSubject.asObservable();
 
-  constructor(private facsimileService: FacsimileService, private apiService: ApiService) {
+  constructor(
+    private facsimileService: FacsimileService, 
+    private apiService: ApiService,
+    private projectService: ProjectService
+  ) {
 
   }
 
@@ -34,7 +39,8 @@ export class FacsimileFileComponent implements AfterViewInit {
   }
 
   loadImage() {
-    this.imagePath = this.facsimileService.getFacsimileImagePath(this.collectionId, this.pageNumber, this.zoom);
+    const currentProject = this.projectService.getCurrentProject();
+    this.imagePath = this.facsimileService.getFacsimileImagePath(this.collectionId, this.pageNumber, currentProject, this.zoom);
     const options = {
       context: new HttpContext().set(SkipLoading, true),
       responseType: 'blob'

src/app/components/file-upload/file-upload.component.ts
@@ -8,6 +8,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { FacsimileService } from '../../services/facsimile.service';
+import { ProjectService } from '../../services/project.service';
 
 enum FileQueueStatus {
   Pending = 'pending',
@@ -46,7 +47,11 @@ export class FileUploadComponent {
   numberOfPages = input.required<number>();
   missingFileNumbers = input.required<number[]>();
 
-  constructor(private facsimileService: FacsimileService, private snackbar: MatSnackBar) { }
+  constructor(
+    private facsimileService: FacsimileService, 
+    private projectService: ProjectService,
+    private snackbar: MatSnackBar
+  ) { }
 
   _queue: FileQueueObject[] = [];
   uploadQueue$: BehaviorSubject<FileQueueObject[]> = new BehaviorSubject<FileQueueObject[]>([]);
@@ -107,7 +112,8 @@ export class FileUploadComponent {
       const formData = new FormData();
       formData.append('facsimile', file, file.name);
 
-      queueObject.request = this.facsimileService.uploadFacsimileFile(this.collectionId(), queueObject.order, formData)
+      const currentProject = this.projectService.getCurrentProject();
+      queueObject.request = this.facsimileService.uploadFacsimileFile(this.collectionId(), queueObject.order, formData, currentProject)
         .subscribe({
           next: (event: any) => { /* eslint-disable-line */
             if (event.type == HttpEventType.UploadProgress) {

src/app/components/translations/translations.component.ts
@@ -9,6 +9,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { BehaviorSubject, filter, Observable, switchMap, take, tap } from 'rxjs';
 
 import { languageOptions, nameForLanguage, Translation, TranslationRequestPost, TranslationResponse } from '../../models/translation';
+import { ProjectService } from '../../services/project.service';
 import { TranslationService } from '../../services/translation.service';
 
 @Component({
@@ -43,7 +44,10 @@ export class TranslationsComponent implements AfterViewInit {
 
   form!: FormGroup;
 
-  constructor(private translationService: TranslationService) { }
+  constructor(
+    private translationService: TranslationService,
+    private projectService: ProjectService
+  ) { }
 
   ngAfterViewInit() {
     this.translationId = this.translationIdd();
@@ -53,7 +57,10 @@ export class TranslationsComponent implements AfterViewInit {
     }
     this.fieldTranslations$ = this.translationLoader$.asObservable().pipe(
       filter(() => this.translationId != null),
-      switchMap(() => this.translationService.getTranslations(this.translationId as number, requestData)),
+      switchMap(() => {
+        const currentProject = this.projectService.getCurrentProject();
+        return this.translationService.getTranslations(this.translationId as number, requestData, currentProject);
+      }),
       tap((translations) => {
         const activeLanguages = translations.map(t => t.language);
         this.filteredLanguages = languageOptions.filter(l => !activeLanguages.includes(l.value));
@@ -86,10 +93,11 @@ export class TranslationsComponent implements AfterViewInit {
     const data = this.form.getRawValue();
 
     let request$: Observable<TranslationResponse>;
+    const currentProject = this.projectService.getCurrentProject();
     if (this.mode() === 'edit') {
-      request$ = this.translationService.editTranslation(this.translationId as number, data);
+      request$ = this.translationService.editTranslation(this.translationId as number, data, currentProject);
     } else {
-      request$ = this.translationService.addTranslation(data);
+      request$ = this.translationService.addTranslation(data, currentProject);
     }
     request$.pipe(take(1)).subscribe({
       next: res => {

commit b312d31771d3d777dfd8bd8141b10fdde34d1c8b

fix: comments, versions, manuscripts, facsimiles not loading on selected publication change
Reloading did not happen because `distinctUntilChanged` only compared if collectionId had changed. The bug was introduced in commit [bd0e088](https://github.com/slsfi/digital-edition-cms-vincent/commit/bd0e088553afa67faf939d367715a2a8db6a4672).

src/app/components/publications/publications.component.ts
@@ -36,7 +36,7 @@ import { LinkTextToPublicationResponse, LinkTextToPublicationRequest, Manuscript
          ManuscriptResponse, Publication, PublicationComment, PublicationCommentResponse,
          PublicationEditRequest, PublicationResponse, Version, VersionResponse,
          XmlMetadata } from '../../models/publication';
-import { cleanEmptyStrings, cleanObject } from '../../utils/utility-functions';
+import { cleanEmptyStrings, cleanObject, shallowArrayEqual } from '../../utils/utility-functions';
 
 @Component({
   selector: 'publications',
@@ -129,7 +129,7 @@ export class PublicationsComponent implements OnInit {
 
     this.comments$ = this.commentLoader$.asObservable().pipe(
       switchMap(() => combineLatest([this.selectedProject$, this.publicationCollectionId$, this.publicationId$]).pipe(
-        distinctUntilChanged(([, prevCollectionId], [, nextCollectionId]) => prevCollectionId === nextCollectionId),
+        distinctUntilChanged(shallowArrayEqual),
         switchMap(([project, collectionId, publicationId]) => {
           if (!project) { return of([]); }
           return this.publicationService.getCommentsForPublication(collectionId as string, publicationId as string, project);
@@ -139,7 +139,7 @@ export class PublicationsComponent implements OnInit {
 
     this.versions$ = this.versionsLoader$.asObservable().pipe(
       switchMap(() => combineLatest([this.selectedProject$, this.publicationCollectionId$, this.publicationId$]).pipe(
-        distinctUntilChanged(([, prevCollectionId], [, nextCollectionId]) => prevCollectionId === nextCollectionId),
+        distinctUntilChanged(shallowArrayEqual),
         switchMap(([project, , publicationId]) => {
           if (!project) { return of([]); }
           return this.publicationService.getVersionsForPublication(publicationId as string, project);
@@ -149,7 +149,7 @@ export class PublicationsComponent implements OnInit {
 
     this.manuscripts$ = this.manuscriptsLoader$.asObservable().pipe(
       switchMap(() => combineLatest([this.selectedProject$, this.publicationCollectionId$, this.publicationId$]).pipe(
-        distinctUntilChanged(([, prevCollectionId], [, nextCollectionId]) => prevCollectionId === nextCollectionId),
+        distinctUntilChanged(shallowArrayEqual),
         switchMap(([project, , publicationId]) => {
           if (!project) { return of([]); }
           return this.publicationService.getManuscriptsForPublication(publicationId as string, project);
@@ -159,7 +159,7 @@ export class PublicationsComponent implements OnInit {
 
     this.facsimiles$ = this.facsimilesLoader$.asObservable().pipe(
       switchMap(() => combineLatest([this.selectedProject$, this.publicationCollectionId$, this.publicationId$]).pipe(
-        distinctUntilChanged(([, prevCollectionId], [, nextCollectionId]) => prevCollectionId === nextCollectionId),
+        distinctUntilChanged(shallowArrayEqual),
         switchMap(([project, , publicationId]) => {
           if (!project) { return of([]); }
           return this.publicationService.getFacsimilesForPublication(publicationId as string, project);

src/app/utils/utility-functions.ts
@@ -46,3 +46,28 @@ export function cleanEmptyStrings<T extends object>(obj: T): T {
   }
   return cleaned as T;
 }
+
+
+/**
+ * Compare two arrays of primitive values (e.g., string, number,
+ * boolean, null, undefined) for shallow equality. Returns true if
+ * both arrays have the same length and each element is strictly equal
+ * (`===`) at the same index.
+ *
+ * This is useful as a comparator for RxJS operators like
+ * `distinctUntilChanged`, when working with `combineLatest` tuples of
+ * primitive values. For example, it prevents duplicate emissions when
+ * all tuple elements are unchanged, even though the arrays are new
+ * references.
+ *
+ * @param a - First array of primitive values
+ * @param b - Second array of primitive values
+ * @returns true if arrays are the same length and all corresponding elements match strictly
+ */
+export function shallowArrayEqual<T extends readonly unknown[]>(a: T, b: T): boolean {
+  if (a.length !== b.length) return false;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}