68153d806f274d67099262ffe5f2a1b43bf30cba
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2025-10-27 13:46:39 +0200

    feature: add app version to login page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -9,7 +9,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { catchError, filter, finalize, map, Observable, of, switchMap, take, tap } from 'rxjs';
+import { catchError, finalize, map, Observable, of, switchMap, take,
+         tap } from 'rxjs';
 
 import { APP_VERSION } from '../../../config/app-version';
 import { LoadingSpinnerComponent } from '../../components/loading-spinner/loading-spinner.component';
@@ -19,37 +20,35 @@ import { ApiService } from './../../services/api.service';
 import { LoadingService } from '../../services/loading.service';
 import { ProjectService } from '../../services/project.service';
 
+
 @Component({
   selector: 'home',
   imports: [
-    CommonModule, MatSelectModule, MatFormFieldModule, MatButtonModule, MatIconModule, MatListModule, RouterLink,
-    MatDividerModule, LoadingSpinnerComponent, MatCardModule
+    CommonModule, RouterLink, MatSelectModule, MatFormFieldModule, MatButtonModule,
+    MatIconModule, MatListModule, MatDividerModule, MatCardModule,
+    LoadingSpinnerComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private readonly apiService = inject(ApiService);
+  private readonly loadingService = inject(LoadingService);
+  private readonly projectService = inject(ProjectService);
+  private readonly snackbar = inject(MatSnackBar);
+
+  availableProjects$: Observable<Project[]> = of([]);
+  environment$ = this.apiService.environment$;
+  loading$ = this.loadingService.loading$
+  repoDetails$: Observable<RepoDetails | null> = of(null);
+  selectedProject$ = this.projectService.selectedProject$;
 
   appVersion = APP_VERSION;
-  availableProjects$: Observable<Project[]>;
-  environment$: Observable<string | null> = new Observable<string | null>();
-  loading$;
   navItems = navigationItems.filter((item) => item.route !== '/');
-  repoDetails$: Observable<RepoDetails | null>;
-  selectedProject$;
   syncingRepo = false;
-
   readonly panelOpenState = signal(false);
 
-  constructor(
-    private apiService: ApiService,
-    private loadingService: LoadingService,
-    private projectService: ProjectService,
-    private snackbar: MatSnackBar
-  ) {
-    this.environment$ = this.apiService.environment$;
-    this.loading$ = this.loadingService.loading$;
-
+  ngOnInit(): void {
     this.availableProjects$ = this.projectService.getProjects().pipe(
       map(projects => projects.sort((a, b) => a.name.localeCompare(b.name))),
 
@@ -65,8 +64,6 @@ export class HomeComponent {
         }
       })
     );
-
-    this.selectedProject$ = this.projectService.selectedProject$;
 
     this.repoDetails$ = this.selectedProject$.pipe(
       switchMap(project => {
